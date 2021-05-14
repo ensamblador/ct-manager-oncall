c3b0cfbf248c2d4682c3aa98a771649260ff2ebd
Enrique Rodriguez <enrique.rodriguez.garrido@gmail.com>
2021-05-14 15:04:28 -0400

    se obtiene el contacto oncall desde base de datos de usuarios

--- _oncall/oncall.py
+++ _oncall/oncall.py
@@ -2,7 +2,9 @@ import json
 from aws_cdk import (
     aws_lambda,
     core,
+    aws_iam,
     aws_ssm as ssm,
+    aws_dynamodb as ddb,
 
 )
 from project_config import (
@@ -12,18 +14,34 @@ from project_config import (
 )
 
 
+from utils.utils import (
+    get_param
+)
+
+tabla_usuarios_ssm = get_param(f'/ct-manager/{STACK_NAME}/voice-mail-api/users-ddb-table')
+
+
+
 class OnCall(core.Construct):
 
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        tabla_usuarios = ddb.Table.from_table_name(self,"tabla_usr_from_ssm",table_name=tabla_usuarios_ssm)
 
         lambda_router = aws_lambda.Function(
             self, "lambdarouter",handler="lambda_function.lambda_handler",
             code=aws_lambda.Code.asset("./lambdas/oncall/contact_router_lambda"),
             description='[ONCALL] Obtiene el numero on call',
             **PYTHON_LAMBDA_CONFIG, 
-            environment=json.loads(json.dumps(dict( UserPhoneNumber ='+56974769647', **BASE_ENV_VARIABLES)))
+            environment=json.loads(json.dumps(dict( TABLE_NAME=tabla_usuarios.table_name, **BASE_ENV_VARIABLES)))
         )
+
+        DDB_POLICY = aws_iam.PolicyStatement(
+            actions=["dynamodb:*"], 
+            resources=[tabla_usuarios.table_arn, '{}/*'.format(tabla_usuarios.table_arn)])
+
+        lambda_router.add_to_role_policy(DDB_POLICY)
 
         ssm.StringParameter(self, "contact-router", 
             description='Lambda que obtiene el numero de la persona oncall',
--- lambdas/oncall/contact_router_lambda/lambda_function.py
+++ lambdas/oncall/contact_router_lambda/lambda_function.py
@@ -1,19 +1,41 @@
 import os
 import datetime
+import boto3
+from boto3.dynamodb.conditions import Key
 
 
 def lambda_handler(event, context):
     print(event)
-    number = os.environ["UserPhoneNumber"]
-    escalation_number = number
-    onCallScheuld = {
-        0:{'TargetContact': number, 'EscalationContact': escalation_number},
-        1:{'TargetContact': number, 'EscalationContact': escalation_number},
-        2:{'TargetContact': number, 'EscalationContact': escalation_number},
-        3:{'TargetContact': number, 'EscalationContact': escalation_number},
-        4:{'TargetContact': number, 'EscalationContact': escalation_number},
-        5:{'TargetContact': number, 'EscalationContact': escalation_number},
-        6:{'TargetContact': number, 'EscalationContact': escalation_number},
-    }
-    dayOfWeek = datetime.datetime.today().weekday()
-    return onCallScheuld[dayOfWeek]
+    TABLE_NAME = os.environ["TABLE_NAME"]
+
+    on_call_contacts = get_on_call_contacts()
+
+    return on_call_contacts
+
+
+def get_on_call_contacts():
+    nombre_tabla = os.environ["TABLE_NAME"]
+    region       = os.environ['REGION']
+    ddb_table = boto3.resource('dynamodb', region_name = region).Table(nombre_tabla)
+
+    items = ddb_table.scan()
+
+    on_call = []
+    for item in items['Items']:
+        if item['oncall'] == True:
+            if item['deliveryOptions']['sms']['phoneNumber'] != '':
+                on_call.append(item)
+
+    respuesta = {}
+
+    if len(on_call):
+        first = on_call[0]
+        respuesta['TargetContact'] = '+{}'.format(first['deliveryOptions']['sms']['phoneNumber'])
+        respuesta['TargetName'] = '{} {}'.format(first['FirstName'], first['LastName'])
+        
+        if len(on_call)>1:
+            for i in range(1, len(on_call)):
+                respuesta[f'TargetContact_{i}'] = '+{}'.format(on_call[i]['deliveryOptions']['sms']['phoneNumber'])
+                respuesta[f'TargetName_{i}'] = '{} {}'.format(on_call[i]['FirstName'], on_call[i]['LastName'])
+    
+    return respuesta
